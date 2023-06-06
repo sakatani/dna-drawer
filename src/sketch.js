691d7be95796b3a5fe39bcf7d8e73b8ae2d8ee87
sakatani <sakatani.yoshihiro@gmail.com>
2023-06-06 15:41:12 +0900

    Linting and comment corrections

--- src/sketch.js
+++ src/sketch.js
@@ -49,11 +49,11 @@ function setup() {
   background(styles.backgroundColor);  
   noStroke();
 
-  // Create Layout GUI
+  // Create DNA Property GUI
   const gui = createGui('DNA Properties');
   gui.addObject(props);
 
-  // Create Shape GUI
+  // Create Style GUI
   const gui2 = createGui('Styles').setPosition(width - 220, 20);
   gui2.addObject(styles);
 
@@ -92,7 +92,6 @@ const linearDNA = () => {
   }
 }
 
-
 class Dna {
   static NUM_AXES = 2;
 
@@ -122,18 +121,14 @@ class Dna {
     const a = 1;
     const b = 1;
       for(let i = 0; i < this.numTurns + 1; i++){
-        //console.log(innerAnchor[i][0],innerAnchor[i][1],outerAnchor[i][0],outerAnchor[i][1]);
         bezier(a * this.innerAnchor[i][0], b * this.innerAnchor[i][1], a * this.innerControlLeft[i][0], b * this.innerControlLeft[i][1], a * this.outerControlLeft[i][0], b * this.outerControlLeft[i][1], a * this.outerAnchor[i][0], b * this.outerAnchor[i][1]);
         if(i != this.numTurns){
-          //console.log(outerAnchor[i][0],outerAnchor[i][1],innerAnchor[i+1][0],innerAnchor[i+1][1]);
           bezier(a * this.outerAnchor[i][0], b * this.outerAnchor[i][1], a * this.outerControlRight[i+1][0], b * this.outerControlRight[i+1][1], a * this.innerControlRight[i+1][0], b * this.innerControlRight[i+1][1], a * this.innerAnchor[i+1][0], b * this.innerAnchor[i+1][1]);         
         }
       }   
-  
     endShape();
     pop();  
   }
-
 }
 
 class CircularDna extends Dna {
@@ -153,13 +148,11 @@ class CircularDna extends Dna {
         this.outerAnchor[i][axis] = this.outerRadius * cos((i * this.pitch + this.pitch / 2 + this.gap) * PI / 180);
         //control point 1 (inner)
         this.innerControlLeft[i][axis] = this.innerRadius * cos((i * this.pitch + this.pitch / 4 + this.gap) * PI / 180);
+        this.innerControlRight[i][axis] = this.innerRadius * cos((i * this.pitch - this.pitch / 4 + this.gap) * PI / 180);
         //control point 2 (outer)
         this.outerControlLeft[i][axis] = this.outerRadius * cos((i * this.pitch + this.pitch / 4 + this.gap) * PI / 180);
-        this.innerControlRight[i][axis] = this.innerRadius * cos((i * this.pitch - this.pitch / 4 + this.gap) * PI / 180);
         this.outerControlRight[i][axis] = this.outerRadius * cos((i * this.pitch - this.pitch / 4 + this.gap) * PI / 180);     
-        //console.log(innerAnchor[i][0],innerAnchor[i][1],outerAnchor[i][0],outerAnchor[i][1]);  
     }
-    //console.log("max_pitch", this.numTurns*pitch);
     
     //y^2 = r^2 - x^2
     let sign = 1;
@@ -179,7 +172,6 @@ class CircularDna extends Dna {
       else sign = 1;
         this.innerControlRight[i][axis] = sign * circle(this.innerRadius, this.innerControlRight[i][0]);
         this.outerControlRight[i][axis] = sign * circle(this.outerRadius, this.outerControlRight[i][0]);
-        //console.log(i, i * this.pitch + this.gap, i * this.pitch + this.gap + this.pitch / 4, i * this.pitch + this.gap - this.pitch / 4); 
     }
     console.log(this.innerControlRight[0][0], this.innerControlRight[0][1], this.outerControlRight[0][0], this.outerControlRight[0][1]);
   }
@@ -195,8 +187,8 @@ class LinearDna extends Dna {
       this.outerAnchor[i][axis] = (i + 1/2) * this.pitch + this.gap;
       //control point
       this.innerControlLeft[i][axis] = (i + 1/4) * this.pitch + this.gap;
-      this.outerControlLeft[i][axis] = (i + 1/4) * this.pitch + this.gap;
       this.innerControlRight[i][axis] = (i - 1/4) * this.pitch + this.gap;
+      this.outerControlLeft[i][axis] = (i + 1/4) * this.pitch + this.gap;
       this.outerControlRight[i][axis] = (i - 1/4) * this.pitch + this.gap;
     }
 
@@ -205,8 +197,8 @@ class LinearDna extends Dna {
       this.innerAnchor[i][axis] = 0;
       this.outerAnchor[i][axis] = this.width;
       this.innerControlLeft[i][axis] = 0;
-      this.outerControlLeft[i][axis] = this.width;
       this.innerControlRight[i][axis] = 0;
+      this.outerControlLeft[i][axis] = this.width;
       this.outerControlRight[i][axis] = this.width;
     }
     console.log(this.innerControlRight[0][0], this.innerControlRight[0][1], this.outerControlRight[0][0], this.outerControlRight[0][1]);
